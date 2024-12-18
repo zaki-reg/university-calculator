455afd259aecd04371d9e7fe3b7383c0a8177aba
zaki-reg <zakig701@gmail.com>
2024-12-18 19:51:13 +0100

    Update UI of welcome.dart

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:uni_calculator_test/custom_shape.dart';
 import '../constants.dart';
 import 'field_year.dart';
 
@@ -18,184 +19,191 @@ class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: backgroundColor,
+      backgroundColor: darkGreen,
       body: LayoutBuilder(
         builder: (context, constraints) {
-          return SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(minHeight: constraints.maxHeight),
-              child: IntrinsicHeight(
-                child: Column(
-                  textDirection: TextDirection.rtl,
-                  children: [
-                    // Top Card Section
-                    Expanded(
-                      child: Card(
-                        margin: EdgeInsets.zero,
-                        elevation: 0,
-                        shape: const BeveledRectangleBorder(
-                          side: BorderSide(width: 1, color: darkGreen),
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(30),
-                            bottomRight: Radius.circular(30),
+          return IntrinsicHeight(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              textDirection: TextDirection.rtl,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30.0, vertical: 10.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 40.0),
+                          child: SvgPicture.asset(
+                            'assets/vectors/logo.svg',
+                            width: 180,
+                            colorFilter: const ColorFilter.mode(
+                                limeGreen, BlendMode.srcATop),
                           ),
                         ),
-                        color: limeGreen,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 30.0, horizontal: 20.0),
-                          child: Center(
-                            child: SvgPicture.asset(
-                              'assets/vectors/logo.svg',
-                              width: 180,
-                              colorFilter: const ColorFilter.mode(
-                                  darkGreen, BlendMode.srcATop),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    // Middle Section
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20.0, vertical: 10.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            // Welcome Text
-                            Directionality(
-                              textDirection: TextDirection.rtl,
-                              child: RichText(
-                                textAlign: TextAlign.right,
-                                text: TextSpan(
-                                  children: [
-                                    TextSpan(
-                                      text: 'مرحباً بك! ',
-                                      style: arabicTextStyle.copyWith(
-                                          color: limeGreen,
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: 45.0,
-                                          height: 1.2),
-                                    ),
-                                    TextSpan(
-                                      text: 'ما الإسم الكريم؟',
-                                      // text: 'كي سمّاك الله؟',
-                                      style: arabicTextStyle.copyWith(
-                                          color: darkGreen,
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: 45.0,
-                                          height: 1.2),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-
-                            const SizedBox(height: 10.0),
-
-                            // Input Row
-                            Row(
-                              textDirection: TextDirection.rtl,
+                        Directionality(
+                          textDirection: TextDirection.rtl,
+                          child: RichText(
+                            textAlign: TextAlign.right,
+                            text: TextSpan(
                               children: [
-                                // Submit Button
-                                InkWell(
-                                  borderRadius: BorderRadius.circular(15.0),
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            CustomDropdownButton(
-                                          enteredText: controller.text,
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                  child: Container(
-                                    width: 60.0,
-                                    height: 60.0,
-                                    decoration: BoxDecoration(
-                                      border: Border.all(
-                                          color: darkGreen, width: 2),
-                                      borderRadius: BorderRadius.circular(15.0),
-                                    ),
-                                    child: const Icon(
-                                      CupertinoIcons.arrow_turn_up_left,
-                                      color: darkGreen,
-                                    ),
-                                  ),
+                                TextSpan(
+                                  text: 'مرحباً بك في ',
+                                  style: arabicTextStyle.copyWith(
+                                      color: limeGreen,
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 45.0,
+                                      height: 1.2),
                                 ),
-
-                                const SizedBox(width: 10),
-
-                                // Text Input
-                                Expanded(
-                                  child: Container(
-                                    height: 60.0,
-                                    decoration: BoxDecoration(
-                                      border: Border.all(
-                                          color: darkGreen, width: 2),
-                                      borderRadius: BorderRadius.circular(15.0),
-                                    ),
-                                    child: Directionality(
-                                      textDirection: TextDirection.rtl,
-                                      child: TextField(
-                                        controller: controller,
-                                        style: textStyle.copyWith(
-                                          color: darkGreen,
-                                          fontSize: 18.0,
-                                        ),
-                                        textAlign: TextAlign.right,
-                                        textDirection: TextDirection.rtl,
-                                        decoration: InputDecoration(
-                                          contentPadding:
-                                              const EdgeInsets.symmetric(
-                                                  horizontal: 20.0),
-                                          hintText: 'أدخِل اسمك هنا',
-                                          hintStyle: arabicTextStyle.copyWith(
-                                            color: Colors.grey,
-                                            fontSize: 18.0,
-                                            fontWeight: FontWeight.w400,
-                                          ),
-                                          border: InputBorder.none,
-                                        ),
-                                        keyboardType: TextInputType.text,
-                                        textInputAction: TextInputAction.done,
-                                        enableInteractiveSelection: true,
-                                        autocorrect: false,
-                                      ),
-                                    ),
+                                TextSpan(
+                                  text: 'نقــــطة! ',
+                                  style: arabicTextStyle.copyWith(
+                                      color: brightYellow,
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 45.0,
+                                      height: 1.2),
+                                ),
+                                TextSpan(
+                                  text: 'مــا إســـــــــمك؟',
+                                  style: arabicTextStyle.copyWith(
+                                    color: limeGreen,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 45.0,
+                                    height: 1.2,
                                   ),
                                 ),
                               ],
                             ),
-                          ],
+                          ),
                         ),
-                      ),
+                        const SizedBox(height: 10.0),
+                      ],
+                    ),
+                  ),
+                ),
+                Stack(
+                  alignment: AlignmentDirectional(0.0, -0.87),
+                  children: [
+                    Stack(
+                      alignment: AlignmentDirectional.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                          child: CustomPaint(
+                            size: const Size(double.infinity, 350),
+                            painter: CustomShapePainter(
+                              edgeRadius: 20,
+                              bigRectHeight: 350,
+                              smallRectHeight: 235,
+                              smallRectWidth: 110 + 15,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
 
-                    const SizedBox(
-                      height: 200,
-                    ),
+                    // submit name button
                     Padding(
-                      padding: const EdgeInsets.only(bottom: 20.0),
-                      child: Text(
-                        'كل الحقوق محفوظة 2025©',
-                        textDirection: TextDirection.rtl,
-                        style: arabicTextStyle.copyWith(
-                          fontSize: 15,
-                          color: darkGreen,
-                        ),
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          // enter your name section
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 15.0),
+                              child: Container(
+                                height: 80.0,
+                                decoration: BoxDecoration(
+                                  color: darkGreen.withAlpha(150),
+                                  border:
+                                      Border.all(color: darkGreen, width: 2),
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                                child: Directionality(
+                                  textDirection: TextDirection.rtl,
+                                  child: TextField(
+                                    textAlignVertical:
+                                        const TextAlignVertical(y: 0.5),
+                                    controller: controller,
+                                    style: arabicTextStyle.copyWith(
+                                      color: darkGreen,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                    textAlign: TextAlign.right,
+                                    textDirection: TextDirection.rtl,
+                                    decoration: InputDecoration(
+                                      contentPadding:
+                                          const EdgeInsets.symmetric(
+                                              horizontal: 20.0),
+                                      hintText: '"زكرياء رقيق"',
+                                      hintStyle: arabicTextStyle.copyWith(
+                                        color: Colors.black.withAlpha(80),
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                      border: InputBorder.none,
+                                    ),
+                                    keyboardType: TextInputType.text,
+                                    textInputAction: TextInputAction.done,
+                                    enableInteractiveSelection: true,
+                                    autocorrect: false,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          InkWell(
+                            borderRadius: BorderRadius.circular(15.0),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => CustomDropdownButton(
+                                    enteredText: controller.text,
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              width: 110.0,
+                              height: 80.0,
+                              decoration: BoxDecoration(
+                                color: brightYellow,
+                                border: Border.all(color: darkGreen, width: 2),
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              child: const Icon(
+                                CupertinoIcons.arrow_turn_up_left,
+                                color: darkGreen,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
                     ),
                   ],
                 ),
-              ),
+
+                // all rights reserved section
+                // Padding(
+                //   padding: const EdgeInsets.only(bottom: 0.0),
+                //   child: Text(
+                //     'كل الحقوق محفوظة 2025©',
+                //     textDirection: TextDirection.rtl,
+                //     style: arabicTextStyle.copyWith(
+                //         fontSize: 15,
+                //         color: limeGreen,
+                //         fontWeight: FontWeight.w500),
+                //   ),
+                // ),
+              ],
             ),
           );
         },

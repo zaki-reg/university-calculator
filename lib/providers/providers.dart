d4c149d5dd0e6e55b87bcf3051c897d2913855f6
zaki-reg <zakig701@gmail.com>
2024-12-21 03:06:48 +0100

    Added userFieldAndYear StateProvider.

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,3 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final usernameProvider = StateProvider<String?>((ref) => '');
+
+final userFieldAndYear = StateProvider<String?>((ref) => '');
